1ce094809a1fcd92033efb3021f22f2faa80ab64
Bảo Thiên <baothien9900@gmail.com>
2024-09-26 23:10:31 +0700

    Enable security

--- src/main/java/vn/lilturtle/jobhunter/controller/HelloController.java
+++ src/main/java/vn/lilturtle/jobhunter/controller/HelloController.java
@@ -1,13 +1,19 @@
 package vn.lilturtle.jobhunter.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import vn.lilturtle.jobhunter.service.error.IdInvalidException;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/")
-    public String getHelloWorld() {
-        return "Hello World (Thiên đẹp trai)";
+    public String getHelloWorld() throws IdInvalidException {
+        if(true){
+            throw new IdInvalidException("Có lỗi đại vương ơi");
+        }
+        return "Hello World!";
     }
 }
