27dbe7257537bffecb167a2452556da9f6a95300
Bảo Thiên <baothien9900@gmail.com>
2024-10-22 15:05:33 +0700

    get Account

--- src/main/java/vn/lilturtle/jobhunter/controller/AuthController.java
+++ src/main/java/vn/lilturtle/jobhunter/controller/AuthController.java
@@ -8,10 +8,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import jakarta.validation.Valid;
 import vn.lilturtle.jobhunter.domain.RestResponse;
@@ -20,6 +17,7 @@ import vn.lilturtle.jobhunter.domain.dto.LoginDTO;
 import vn.lilturtle.jobhunter.domain.dto.ResLoginDTO;
 import vn.lilturtle.jobhunter.service.UserService;
 import vn.lilturtle.jobhunter.util.SecurityUtil;
+import vn.lilturtle.jobhunter.util.annotation.ApiMessage;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -40,7 +38,7 @@ public class AuthController {
         this.userService = userService;
     }
 
-    @PostMapping("/login")
+    @PostMapping("/auth/login")
     public ResponseEntity<ResLoginDTO> login(@Valid @RequestBody LoginDTO loginDTO) {
         // Nạp input gồm username/password vào Security
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
@@ -49,8 +47,6 @@ public class AuthController {
         Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
 
 
-        //create token
-        String access_token = this.securityUtil.createAccessToken(authentication);
         // nạp thông tin (nếu xử lý thành công) vào SecurityContext
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
@@ -63,6 +59,9 @@ public class AuthController {
             ResLoginDTO.UserLogin userLogin = new ResLoginDTO.UserLogin(currentUserDB.getId(), currentUserDB.getEmail(), currentUserDB.getName());
             res.setUser(userLogin);
         }
+
+        //create token
+        String access_token = this.securityUtil.createAccessToken(authentication, res.getUser());
 
         res.setAccessToken(access_token);
 
@@ -84,6 +83,23 @@ public class AuthController {
         return ResponseEntity.ok()
                 .header(HttpHeaders.SET_COOKIE, resCookies.toString())
                 .body(res);
+    }
+
+    @GetMapping("/auth/account")
+    @ApiMessage("Fetch account")
+    public ResponseEntity<ResLoginDTO.UserLogin> getAccount() {
+        String email = SecurityUtil.getCurrentUserLogin().isPresent() ?
+                SecurityUtil.getCurrentUserLogin().get() : " ";
+
+        User currentUserDB = this.userService.handleGetUserByUserName(email);
+        ResLoginDTO.UserLogin userLogin = new ResLoginDTO.UserLogin();
+
+        if (currentUserDB != null) {
+            userLogin.setId(currentUserDB.getId());
+            userLogin.setEmail(currentUserDB.getEmail());
+            userLogin.setName(currentUserDB.getName());
+        }
+        return ResponseEntity.ok().body(userLogin);
     }
 
 }
