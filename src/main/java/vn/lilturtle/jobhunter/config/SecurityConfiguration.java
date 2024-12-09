50261733912bfdd5609acdaf4fe6298d7ca56aa3
Bảo Thiên <baothien9900@gmail.com>
2024-12-09 22:22:18 +0700

    update2

--- src/main/java/vn/lilturtle/jobhunter/config/SecurityConfiguration.java
+++ src/main/java/vn/lilturtle/jobhunter/config/SecurityConfiguration.java
@@ -48,7 +48,7 @@ public class SecurityConfiguration {
                 "/",
                 "/api/v1/auth/login", "/api/v1/auth/refresh", "/api/v1/auth/register",
                 "/storage/**",
-                "/api/v1/companies/**", "/api/v1/jobs/**"
+                "/api/v1/email/**",
         };
 
         http
@@ -60,7 +60,7 @@ public class SecurityConfiguration {
                                 .requestMatchers(HttpMethod.GET, "/api/v1/companies/**").permitAll()
                                 .requestMatchers(HttpMethod.GET, "/api/v1/jobs/**").permitAll()
                                 .requestMatchers(HttpMethod.GET, "/api/v1/skills/**").permitAll()
-                                
+
                                 .anyRequest().authenticated())
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults())
                         .authenticationEntryPoint(customAuthenticationEntryPoint))
